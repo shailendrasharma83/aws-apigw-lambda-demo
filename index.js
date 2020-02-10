d19a99ebfc44d67b04e3c51c6e072e871723e7a9
Shailendra Sharma <shailendra.sharma@centrica.com>
2020-02-10 23:51:21 +0000

    Added handler code

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const AWS = require('aws-sdk')
+const SSM = new AWS.SSM()
 
 exports.handler = async function (event) {
+    let map, amount;
+    try {
+        amount = event.queryStringParameters.amount
 
+        const SSM_PARAMS = await SSM.getParameter({
+            Name: "rate-map",
+            WithDecryption: true
+        }).promise()
+
+        map = JSON.parse(SSM_PARAMS.Parameter.Value)
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({"rate" : getApplicableRate(map.rates, amount)}),
+        };
+    } catch (err) {
+        console.log('Error Occurred ::' + err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ detail: err }),
+        };
+    }
+}
+
+function getApplicableRate(rates, amount) {
+    return rates.reduce((prev, current) => current.min <= amount ? current : prev).rate
 }
